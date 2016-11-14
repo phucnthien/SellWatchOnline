6bb2001ba936e3ec60cb1bb1249fe7f9f0c7f844
DESKTOP-GO9L1QK\Nguyen Thien Phuc <phucnthien@gmail.com>
2016-11-14 19:02:50 +0700

    add css/site.css

--- advanced/backend/assets/AppAsset.php
+++ advanced/backend/assets/AppAsset.php
@@ -13,7 +13,7 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
        
-       
+        'css/site.css',
         'assets/css/font-awesome.css',
         'assets/css/basic.css',
         'assets/css/custom.css',
