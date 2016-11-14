137987cb745daf5d24aae1957b65303d089480fa
DESKTOP-GO9L1QK\Nguyen Thien Phuc <phucnthien@gmail.com>
2016-11-14 18:36:08 +0700

    no message

--- advanced/backend/assets/AppAsset.php
+++ advanced/backend/assets/AppAsset.php
@@ -12,9 +12,17 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css',
+       
+       
+        'assets/css/font-awesome.css',
+        'assets/css/basic.css',
+        'assets/css/custom.css',
     ];
     public $js = [
+        
+        'assets/js/bootstrap.js',
+        'assets/js/jquery.metisMenu.js',
+        'assets/js/custom.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
